d2a3c21cd973740b3c5e29e5f4f47105825cf53e
Ore19095 <ore19095@uvg.edu.gt>
2023-11-13 00:07:09 -0600

    agregado pasajeros

--- home/models.py
+++ home/models.py
@@ -6,7 +6,8 @@ class Travels(models.Model):
     time = models.TimeField()
     to = models.CharField(max_length=50)
     driver = models.ForeignKey('login.User', on_delete=models.CASCADE)
-    free_seat = models.IntegerField(default=0)
+    free_seats = models.IntegerField(default=0)
+    passengers = models.ManyToManyField('login.User', related_name='passengers')
     # create a string representation of the user model
     def __str__(self):
         return self.person.username + ' ' + self.to + ' ' + str(self.date) + ' ' + str(self.time)
