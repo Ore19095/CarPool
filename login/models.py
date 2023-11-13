0831942466bb507531d529e6f3336fd42500f565
Ore19095 <ore19095@uvg.edu.gt>
2023-11-12 22:04:18 -0600

    modelo de usuario actualizado

--- login/models.py
+++ login/models.py
@@ -8,6 +8,9 @@ class User(models.Model):
     password = models.CharField(max_length=50)
     email = models.EmailField(max_length=50)
     phone = models.CharField(max_length=8)
+    is_passenger = models.BooleanField(default=True)
+    car_type = models.CharField(max_length=50, default='None')
+    car_branch = models.CharField(max_length=50, default='None')
     # create a string representation of the user model
     def __str__(self):
         return self.username + " " + self.password + " " + self.email + " " + self.phone #+ " " + self.address + " " + self.city + " " + self.state + " " + self.zipcode + " " + self.country
